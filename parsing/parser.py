155ad3fd7af0d3cba7c2d59e8e466c9890a23175
Daniel Hader <dhader96@gmail.com>
2025-07-03 18:50:17 -0500

    grouped tokenizer logic and implemented shunting yard

--- parsing/parser.py
+++ parsing/parser.py
@@ -1,87 +1,90 @@
-from dataclasses import dataclass
-from enum import Enum
 import math
-import re
+from syntax_tree import Tokenizer, Token
 
-class TokenKind(Enum):
-    NUMBER = 0
-    PAREN_L = 1
-    PAREN_R = 2
-    CONSTANT = 3
-    OPERATOR = 4
-    FUNCTION = 5
-
-@dataclass
-class Token:
-    symbol: str
-    kind: TokenKind
-
-def starts_with_any(expr: str, symbs: list[str]) -> str | None:
-    for s in symbs:
-        if expr.startswith(s):
-            return s
-    return None
-
-def tokenize(expr: str, **kwargs):
-    left_parens  = list(map(lambda x: x.lower(), kwargs.get('left_parens',  ['(', '['])))
-    right_parens = list(map(lambda x: x.lower(), kwargs.get('right_parens', [')', ']'])))
-    constants    = list(map(lambda x: x.lower(), kwargs.get('constants',    ['pi'])))
-    operators    = list(map(lambda x: x.lower(), kwargs.get('operators',    ['+', '-', '*', '/'])))
-    functions    = list(map(lambda x: x.lower(), kwargs.get('functions',    ['sin', 'cos'])))
-
-    tokens = []
-
-    while len(expr) > 0:
-        if symb := starts_with_any(expr, left_parens):
-            tokens.append(Token(symb, TokenKind.PAREN_L))
-            expr = expr.removeprefix(symb)
-        elif symb := starts_with_any(expr, right_parens):
-            tokens.append(Token(symb, TokenKind.PAREN_R))
-            expr = expr.removeprefix(symb)
-        elif symb := starts_with_any(expr, constants):
-            tokens.append(Token(symb, TokenKind.CONSTANT))
-            expr = expr.removeprefix(symb)
-        elif symb := starts_with_any(expr, operators):
-            tokens.append(Token(symb, TokenKind.OPERATOR))
-            expr = expr.removeprefix(symb)
-        elif symb := starts_with_any(expr, functions):
-            tokens.append(Token(symb, TokenKind.PAREN_R))
-            expr = expr.removeprefix(symb)
-        else:
-            number_regex = re.compile(r'\-?\d*\.?\d+')
-            match = number_regex.match(expr)
-            if match:
-                symb = match.group(0)
-                tokens.append(Token(symb, TokenKind.NUMBER))
-                expr = expr.removeprefix(symb)
-            else:
-                raise Exception(f'could not parse expression "{expr}"')
-        expr = expr.lstrip()
-    return tokens
-
-operator_table = {
-    '+': lambda x, y: x + y,
-    '-': lambda x, y: x - y,
-    '*': lambda x, y: x * y,
-    '/': lambda x, y: x / y,
-}
-
-function_table = {
-    'pi': (0, lambda: math.pi),
-    'sin': (1, lambda x : math.sin(x)),
-}
-
+operators = ['+', '-', '*', '/', '^']
+functions = ['pi', 'sqrt', 'sin', 'cos']
 left_parens = ['(', '[']
 right_parens = [')', ']']
 
+function_arity = {
+    'pi': 0,
+    'sqrt': 1,
+    'sin': 2,
+    'cos': 2,
+}
 
-def parse(expr: str):
-    pass
+operator_left_assoc = {
+    '+': True,
+    '-': True,
+    '*': True,
+    '/': True,
+    '^': False,
+}
+
+operator_precidence = {
+    '+': 0,
+    '-': 0,
+    '*': 1,
+    '/': 1,
+    '^': 2,
+}
+
+def parse(tokens: list[Token]):    
+    stack = []
+    output = []
+
+    for token in tokens:
+        if token.type == 'number':
+            output.append(token)
+        
+        elif token.type == 'function':
+            stack.append(token)
+        
+        elif token.type == 'comma':
+            
+            while stack[-1].type != 'left paren':
+                output.append(stack.pop())
+
+        elif token.type == 'left paren':
+            stack.append(token)
+
+        elif token.type == 'right paren':
+            while stack[-1].type != 'left paren':
+                output.append(stack.pop())
+            assert stack[-1].type == 'left paren'
+            stack.pop()
+
+        else: # an operator
+            while len(stack) > 0 and stack[-1].type != 'left paren':
+                if stack[-1].precedence > token.precedence or (stack[-1].precedence == token.precedence and token.left_associative):
+                    output.append(stack.pop())
+                else:
+                    break
+            stack.append(token)
+
+    while len(stack) > 0:
+        top = stack.pop()
+        assert top.type != 'left paren'
+        output.append(top)
+
+    return output
 
 if __name__ == "__main__":
     
-    expr = '1 * (2 + 3) + sin(2 * pi)'
-    tokens = tokenize(expr)
+    tokenizer = Tokenizer()
+    tokenizer.register_operator('+',  True, 0, lambda x, y: x + y)
+    tokenizer.register_operator('-',  True, 0, lambda x, y: x - y)
+    tokenizer.register_operator('*',  True, 1, lambda x, y: x * y)
+    tokenizer.register_operator('/',  True, 1, lambda x, y: x / y)
+    tokenizer.register_operator('^', False, 2, lambda x, y:  x**y)
+
+    expr = '1 * (2 + 3) + (2 * -5.2)'
+    tokens = tokenizer.tokenize(expr)
 
     for token in tokens:
+        print(token)
+
+    print()
+
+    for token in parse(tokens):
         print(token)
--- parsing/syntax_tree.py
+++ parsing/syntax_tree.py
@@ -1,8 +1,15 @@
 from __future__ import annotations
 from decimal import Decimal, getcontext
+from dataclasses import dataclass
 from inspect import signature
+import re
 
 getcontext().prec = 50
+
+@dataclass
+class Token:
+    symbol: str
+    type: str
 
 class ASTNode:
 
@@ -41,8 +48,7 @@ class ASTNode:
             if not callable(func):
                 raise Exception(f'function "{self.value}" in function_map cannot be called as a function')
             
-            sig = signature(func)
-            num_args = len(sig.parameters)
+            num_args = len(signature(func).parameters)
             
             if num_args != len(self.children):
                 raise Exception(f'function "{self.value}" in function_map expects {num_args} arguments, received {len(self.children)}')
@@ -70,6 +76,87 @@ class ASTNode:
 
     def __str__(self):
         return self.__to_string('', '', '')
+
+class Tokenizer:
+
+    def __init__(self):
+        self.function_map = {}
+        self.function_arity = {}
+
+        self.operator_map = {}
+        self.operator_left_assoc = {}
+        self.operator_prec = {}
+
+        self.left_parens = ['(', '[']
+        self.right_parens = [')', ']']
+        self.commas = [',']
+
+        self.used_symbols = set(self.left_parens + self.right_parens)
+
+    def register_function(self, symbol, arity, func):
+        num_args = len(signature(func).parameters)
+        if arity != num_args:
+            raise Exception(f'failed to register function "{symbol}", expecting arity {arity}, found {num_args}')
+        if symbol in self.used_symbols:
+            raise Exception(f'cannot register function "{symbol}", symbol already registered')
+        
+        self.function_map[symbol] = func
+        self.function_arity[symbol] = arity
+        self.used_symbols.add(symbol)
+
+    def register_operator(self, symbol, left_assoc, precedence, func):
+        num_args = len(signature(func).parameters)
+        if num_args != 2:
+            raise Exception(f'operator must use a function of arity 2, found {num_args}')
+        if symbol in self.function_map:
+            raise Exception(f'cannot register operator "{symbol}", symbol already registered')
+        
+        self.operator_map[symbol] = func
+        self.operator_left_assoc[symbol] = left_assoc
+        self.operator_prec[symbol] = precedence
+        self.used_symbols.add(symbol)
+
+    def tokenize(self, expr: str):
+        tokens = []
+
+        def starts_with_any(expr: str, symbs: list[str]) -> str | None:
+            for s in symbs:
+                if expr.startswith(s):
+                    return s
+            return None
+        
+        number_regex = re.compile(r'^\-?\d*\.?\d+')
+
+        while len(expr) > 0:
+            match = number_regex.match(expr)
+            if match:
+                symb = match.group(0)
+                tokens.append(Token(symb, 'number'))
+                expr = expr.removeprefix(symb)
+            elif symb := starts_with_any(expr, self.left_parens):
+                tokens.append(Token(symb, 'left paren'))
+                expr = expr.removeprefix(symb)
+            elif symb := starts_with_any(expr, self.right_parens):
+                tokens.append(Token(symb, 'right paren'))
+                expr = expr.removeprefix(symb)
+            elif symb := starts_with_any(expr, self.commas):
+                tokens.append(Token(symb, 'comma'))
+                expr = expr.removeprefix(symb)
+            elif symb := starts_with_any(expr, self.operator_map.keys()):
+                token = Token(symb, 'operator')
+                token.precedence = self.operator_prec[symb]
+                token.left_associative = self.operator_left_assoc[symb]
+                tokens.append(token)
+                expr = expr.removeprefix(symb)          
+            elif symb := starts_with_any(expr, self.function_map.keys()):
+                token = Token(symb, 'function')
+                token.arity = self.function_arity[symb]
+                tokens.append(token)
+                expr = expr.removeprefix(symb)
+            else:
+                raise Exception(f'could not parse expression "{expr}"')
+            expr = expr.lstrip()
+        return tokens
 
 def test():
     root = ASTNode('function', 'add', [
